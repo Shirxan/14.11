8470c2b58751ed7b00d912d635ab44970f234f77
abilov.w <109854878+Shirxan@users.noreply.github.com>
2022-11-14 18:30:06 +0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import SectionVue from './components/Section.vue';
+</script>
+<template>
+  <div class="container">
+
+    <SectionVue/>
+  </div>
+</template>
---
+++ src/components/Section.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container d-flex">
+    <h1>Player:{{ count }}</h1>
+    <h2 class="offset-3">Wolf:{{ count1 }}</h2>
+  </div>
+  <div class="btn-group" role="group" aria-label="Default button group">
+    <button id="hucum" @click="hucum()" type="button" class="btn btn-outline-dark">
+      Hücüm
+    </button>
+    <button id="qetihucum" @click="qetihucum()" type="button" class="btn btn-outline-dark">
+      Şiddətli hücüm
+    </button>
+    <button @click="son()" type="button" class="btn btn-outline-dark">Son</button>
+  </div>
+
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      count: 100,
+      count1: 100,
+      point : 0,
+      point1 :0 
+    };
+  },
+  methods: {
+    hucum() {
+      if (this.count>0) {
+        this.count-=10
+        this.point1+= 1
+      } else {
+        this.count = 0;
+      }
+    },
+    qetihucum(){
+      if(this.count1>0 ){
+      this.count1-=25
+      this.point+=1
+    }else if(this.count1>0){
+     this.count1=0
+    }
+    },
+    son(){
+      location.reload()
+    }
+  },
+  watch:{
+    point:function(){
+      if(this.point==2){
+        document.querySelector('#qetihucum').setAttribute("class","btn disabled")
+      }
+    },
+    point1:function(){
+      if(this.point1==7){
+        document.querySelector('#hucum').setAttribute("class","btn disabled")
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.btn-group {
+  margin-left: 223px;
+  margin-top: 40px;
+}
+</style>
